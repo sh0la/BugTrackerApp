171bc02fb2906756016949fe13bb1106e09bf6ed
shola <sholaadeniyi@gmail.com>
2020-06-16 15:55:50 -0500

    Add project files.

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugTrackerApp.Startup))]
+namespace BugTrackerApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

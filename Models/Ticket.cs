c22a1c805fc5762ad8c3c4d27b6012182503bf75
shola <sholaadeniyi@gmail.com>
2020-06-16 16:31:40 -0500

    create ticket model class

---
+++ Models/Ticket.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Common.CommandTrees;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Updated { get; set; }
+        public int ProjectId { get; set; }
+        public virtual Project Project { get; set; }
+        public int TicketTypeId { get; set; }
+        public virtual TicketType TicketType { get; set; }
+        public int TicketPriorityId { get; set; }
+        public virtual TicketPriority TicketTypePriority { get; set; }
+        public int TicketStatusId { get; set; }
+        public virtual TicketStatus TicketStatus { get; set; }
+        public int OwnerUserId { get; set; }
+        public virtual OwnerUser OwnerUser { get; set; }
+        public int AssignedToUserId { get; set; }
+        public virtual AssignedToUser AssignedToUser { get; set; }
+        public ICollection<TicketAttachment> TicketAttachments { get; set; }
+        public ICollection<TicketHistory> TicketHistories { get; set; }
+        public ICollection<TicketNotification> TicketNotifications { get; set; }
+
+        public Ticket()
+        {
+            this.TicketAttachments = new HashSet<TicketAttachment>();
+            this.TicketHistories = new HashSet<TicketHistory>();
+            this.TicketNotifications = new HashSet<TicketNotification>();
+        }
+
+    }
+
+}

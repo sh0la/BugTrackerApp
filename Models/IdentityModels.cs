86c140067ea4b98bef3cb8c0fca5de65a12593a6
shola <sholaadeniyi@gmail.com>
2020-06-16 16:06:57 -0500

    Modify connection string

--- Models/IdentityModels.cs
+++ Models/IdentityModels.cs
@@ -21,7 +21,7 @@ namespace BugTrackerApp.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("BugTrackerConnectionString", throwIfV1Schema: false)
         {
         }
 

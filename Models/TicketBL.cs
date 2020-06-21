48a7d50cdd56681fd14e8a15bdc3b139b2d1409a
shola <sholaadeniyi@gmail.com>
2020-06-21 05:02:37 -0500

    create TicketDAL and TicketBL classes

---
+++ Models/TicketBL.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models
+{
+    public class TicketBL
+    {
+        private readonly TicketDAL _repo;
+
+        public TicketBL(TicketDAL repo)
+        {
+            _repo = repo;
+        }
+
+        public Ticket GetTicket(int Id)
+        {
+            return _repo.GetTicket(Id);
+
+        }
+
+        public void AddTicket(Ticket ticket)
+        {
+            if (ticket != null)
+                _repo.Add(ticket);
+        }
+
+        public void EditTicket(int Id)
+        {
+            var ticket = _repo.GetTicket(Id);
+            _repo.Edit(ticket);
+        }
+    }
+}
---
+++ Models/TicketDAL.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BugTrackerApp.Models
+{
+    public class TicketDAL
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        
+
+        public string Name { get; set; }
+
+        public void Add(Ticket ticket)
+        {
+            db.Tickets.Add(ticket);
+            db.SaveChanges();
+        }
+
+        public Ticket GetTicket(int Id)
+        {
+            return db.Tickets.Find(Id);
+
+        }
+
+        public void Edit(Ticket ticket)
+        {
+            //var currentTicket = db.Tickets.Find(Id);
+            db.Entry(ticket).State = EntityState.Modified;
+            db.SaveChanges();
+            
+        }
+
+    }
+}

94cb8bcaaf4ef0292565986a0a91e875ac766d78
shola <sholaadeniyi@gmail.com>
2020-06-22 11:32:17 -0500

    make updates

---
+++ Models/TicketDAL.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace BugTrackerApp.Models
+{
+    public class TicketDAL
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+        
+
+        public void Add(Ticket ticket)
+        {
+            db.Tickets.Add(ticket);
+            db.SaveChanges();
+        }
+
+        public Ticket GetTicket(int Id)
+        {
+            return db.Tickets.Find(Id);
+
+        }
+
+        public void Edit(Ticket ticket)
+        {
+            db.Entry(ticket).State = EntityState.Modified;
+            db.SaveChanges();
+            
+        }
+
+    }
+}
---
+++ Models/UserDAL.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.Ajax.Utilities;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models
+{
+    public class UserDAL
+    {
+        static ApplicationDbContext db = new ApplicationDbContext();
+        static UserManager<ApplicationUser> userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(db));
+        static RoleManager<IdentityRole> roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(db));
+
+    
+        public static void CreateRole(string roleName)
+        {
+            if (!roleManager.RoleExists(roleName))
+            {
+                roleManager.Create(new IdentityRole
+                {
+                    Name = roleName
+                });
+
+            }
+        }
+
+        public static void DeleteRole(string roleName)
+        {
+            
+            // can we delete default roles?
+            if (roleManager.RoleExists(roleName))
+            {
+                roleManager.Delete(new IdentityRole { Name = roleName });
+            }
+        }
+
+        public static bool UserIsInRole(string userId, string roleName)
+        {
+            return userManager.IsInRole(userId, roleName);
+        }
+
+        public static bool RemoveUserFromRole(string userId, string roleName)
+        {
+            if (UserIsInRole(userId, roleName))
+            {
+                userManager.RemoveFromRole(userId, roleName);
+                return true;
+            }
+
+            return false;
+        }
+
+        public static void AddUserToRole(string roleName, string userName)
+        {
+            IdentityRole role = roleManager.FindByName(roleName);
+            ApplicationUser user = userManager.FindByName(userName);
+            if (role != null && user != null && !userManager.IsInRole(user.Id, roleName))
+            {
+                userManager.AddToRole(user.Id, roleName);
+            }
+        }
+
+        public static void RemoveUser(ApplicationUser user)
+        {
+            db.Users.Remove(user);
+            db.SaveChanges();
+        }
+
+
+    }
+}

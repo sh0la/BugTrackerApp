f429cd53e1a599ac5b2589fc956cd81f287f2977
shola <sholaadeniyi@gmail.com>
2020-06-22 11:32:51 -0500

    Add UserDAL

---
+++ Models/UserBL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models
+{
+    public class UserBL
+    {
+        UserDAL _repo;
+
+        public UserBL(UserDAL repo)
+        {
+            _repo = repo;
+        }
+
+       
+
+
+
+
+    }
+}

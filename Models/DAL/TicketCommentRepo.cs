5d27c9deca12ac704d1d5658c987fac9d0c97686
Zane Paton <zanerepaton@gmail.com>
2020-06-23 14:09:25 -0500

    Create Comment Repo

---
+++ Models/DAL/TicketCommentRepo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.DAL
+{
+    public class TicketCommentRepo
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+        public void Add(TicketComment comment)
+        {
+            db.TicketComments.Add(comment);
+            db.SaveChanges();
+        }
+        public void Delete(int id)
+        {
+            var commentToDelete = db.TicketComments.Find(id);
+            db.TicketComments.Remove(commentToDelete);
+            db.SaveChanges();
+        }
+        public TicketComment Get(int id)
+        {
+            return db.TicketComments.Find(id);
+
+        }
+        
+    }
+}

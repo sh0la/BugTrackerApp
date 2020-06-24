a9b937c24192599ecdfcae597084080d429c4cec
shola <sholaadeniyi@gmail.com>
2020-06-24 07:56:50 -0500

    add create comment to ticket comment repo

--- Models/DAL/TicketCommentRepo.cs
+++ Models/DAL/TicketCommentRepo.cs
@@ -8,17 +8,28 @@ namespace BugTrackerApp.Models.DAL
     public class TicketCommentRepo
     {
         private ApplicationDbContext db = new ApplicationDbContext();
-        public void Add(TicketComment comment)
+
+        public void Create(Ticket ticket, ApplicationUser user, string comment)
         {
-            db.TicketComments.Add(comment);
+            var ticketComment = new TicketComment();
+            ticketComment.TicketId = ticket.Id;
+            ticketComment.Comment = comment;
+            ticketComment.ApplicationUserId = user.Id;
+        }
+
+        public void Add(TicketComment ticketComment)
+        {
+            db.TicketComments.Add(ticketComment);
             db.SaveChanges();
         }
+
         public void Delete(int id)
         {
             var commentToDelete = db.TicketComments.Find(id);
             db.TicketComments.Remove(commentToDelete);
             db.SaveChanges();
         }
+
         public TicketComment Get(int id)
         {
             return db.TicketComments.Find(id);

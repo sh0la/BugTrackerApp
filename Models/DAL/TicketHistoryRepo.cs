c0fd7f1e7508de3cceecd12b47732a3edc620002
shola <sholaadeniyi@gmail.com>
2020-06-24 06:41:18 -0500

    start tickethistory repo

---
+++ Models/DAL/TicketHistoryRepo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.DAL
+{
+    public class TicketHistoryRepo
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public void Add(Ticket ticket)
+        {
+            var ticketHistory = new TicketHistory();
+            var currentTicketHistory = db.TicketHistories.Find(ticket.Id);
+            //Get the last history
+            //compare the properties
+            //update the new history
+            
+        }
+    }
+}

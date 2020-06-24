5710b7e2b766014978a3f8b78e6f229daf7734e6
shola <sholaadeniyi@gmail.com>
2020-06-24 08:31:58 -0500

    create project BL

---
+++ Models/BL/ProjectBL.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.BL
+{
+    public class ProjectBL
+    {
+        private ProjectRepo _repo;
+
+        public ProjectBL(ProjectRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public Project GetProject(int Id)
+        {
+            return _repo.Get(Id);
+        }
+
+        public IList<Project> GetAllProjects()
+        {
+            return _repo.Get();
+        }
+
+        public void CreateAProject(string name)
+        {
+            _repo.Create(name);
+        }
+
+        public void EditAProject(string name)
+        {
+            _repo.Edit(name);
+        }
+
+
+    }
+}

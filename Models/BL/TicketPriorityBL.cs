b88b3630c1a492ee363226efc2760d8f05ee18a0
shola <sholaadeniyi@gmail.com>
2020-06-24 07:19:53 -0500

    create ticket priority DAL and BL

---
+++ Models/BL/TicketPriorityBL.cs
@@ -0,0 +1,46 @@
+﻿using BugTrackerApp.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.BL
+{
+    public class TicketPriorityBL
+    {
+        TicketPriorityRepo _repo;
+
+        public TicketPriorityBL(TicketPriorityRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public void CreatePriority(string name)
+        {
+            _repo.Add(name);
+        }
+
+        public void EditPriorityName(int Id)
+        {
+            _repo.Edit(Id);
+        }
+
+        public void EditPriority(TicketPriority ticketPriority)
+        {
+            _repo.Edit(ticketPriority);
+        }
+
+        public IList<TicketPriority> GetTicketPriorities()
+        {
+            return _repo.Get();
+        }
+
+
+        public void DeleteTicketPriorityLabel(int Id)
+        {
+            _repo.Delete(Id);
+        }
+
+
+    }
+}
---
+++ Models/DAL/TicketPriorityRepo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.DAL
+{
+    public class TicketPriorityRepo
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+        public void Add(string name)
+        {
+            var ticketPriority = new TicketPriority();
+            ticketPriority.Name = name;
+            db.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            var currentTicketPriority = db.TicketPriorities.Find(Id);
+            db.TicketPriorities.Remove(currentTicketPriority);
+
+        }
+
+        public void Edit(int Id)
+        {
+            var currentTicketPriority = db.TicketPriorities.Find(Id);
+            db.Entry(currentTicketPriority).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public void Edit(TicketPriority currentTicketPriority)
+        {
+            db.Entry(currentTicketPriority).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+
+        public IList<TicketPriority> Get()
+        {
+            return db.TicketPriorities.ToList();
+        }
+    }
+}

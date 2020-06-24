4bca4325eb12250d2f289f33365f5f4474e62446
shola <sholaadeniyi@gmail.com>
2020-06-24 07:44:20 -0500

    create ticket types BL and DAL

---
+++ Models/BL/TicketTypeBL.cs
@@ -0,0 +1,46 @@
+﻿using BugTrackerApp.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.BL
+{
+    public class TicketTypeBL
+    {
+        TicketTypeRepo _repo;
+
+        public TicketTypeBL(TicketTypeRepo repo)
+        {
+            _repo = repo;
+        }
+
+        public IList<TicketType> GetAllTicketTypes()
+        {
+            return _repo.GetAll();
+        }
+
+        public void CreateTicketTypeName(string name)
+        {
+            _repo.Add(name);
+        }
+
+        public void EditTicketTypeName(TicketType ticketType)
+        {
+            _repo.Edit(ticketType);
+        }
+
+        public void DeleteTicketTypeName(int Id)
+        {
+            _repo.Delete(Id);
+        }
+
+        public void DeleteTicketTypeName(TicketType ticketType)
+        {
+            _repo.Delete(ticketType);
+        }
+
+
+
+    }
+}
---
+++ Models/DAL/TicketTypeRepo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.UI.HtmlControls;
+
+namespace BugTrackerApp.Models.DAL
+{
+    public class TicketTypeRepo
+    {
+        ApplicationDbContext db = new ApplicationDbContext();
+
+
+        public IList<TicketType> GetAll()
+        {
+            return db.TicketTypes.ToList();
+        }
+
+        public void Add(string name)
+        {
+            var ticketType = new TicketType { Name = name };
+            db.SaveChanges();
+        }
+
+        public void Delete(int Id)
+        {
+            var currentTicketType = db.TicketTypes.Find(Id);
+            db.TicketTypes.Remove(currentTicketType);
+            db.SaveChanges();
+        }
+
+        public void Delete(TicketType ticketType)
+        {
+            db.TicketTypes.Remove(ticketType);
+        }
+
+        public void Edit(TicketType ticketType)
+        {
+            db.Entry(ticketType).State = EntityState.Modified;
+            db.SaveChanges();
+        }
+    }
+}

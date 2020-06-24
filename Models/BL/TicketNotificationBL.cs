aa12d93ea2d0bb12a24fb5def25b092bed98d0a0
shola <sholaadeniyi@gmail.com>
2020-06-24 05:38:01 -0500

    create add method for ticket notification

---
+++ Models/BL/TicketNotificationBL.cs
@@ -0,0 +1,25 @@
+﻿using BugTrackerApp.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.BL
+{
+    public class TicketNotificationBL
+    {
+        TicketNotificationRepo _repo;
+
+        public TicketNotificationBL(TicketNotificationRepo repo)
+        {
+            _repo = repo;
+        }
+
+
+        public void AddNotification(Ticket ticket, ApplicationUser user)
+        {
+            _repo.Add(ticket, user);
+        }
+
+    }
+}
---
+++ Models/DAL/TicketNotificationRepo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BugTrackerApp.Models.DAL
+{
+    public class TicketNotificationRepo
+    {
+        static ApplicationDbContext db = new ApplicationDbContext();
+        
+        public void Add(Ticket ticket, ApplicationUser user)
+        {
+            var ticketNotification = new TicketNotification();
+            ticketNotification.ApplicationUserId = user.Id;
+            ticketNotification.TicketId = ticket.Id;
+            db.SaveChanges();
+        }
+
+
+
+
+
+    }
+}
